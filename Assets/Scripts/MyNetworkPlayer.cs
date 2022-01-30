d36284faeef1b3445d942fa4a841e1cbcc5912fc
QingOu <qingqingou@gmail.com>
2022-01-30 18:51:34 -0500

    Add Remote Actions, Client call server, vice versa

--- Assets/Scripts/MyNetworkPlayer.cs
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -17,6 +17,7 @@ public class MyNetworkPlayer : NetworkBehaviour
   [SerializeField]
   private Color playerColor = Color.black;
 
+  #region Server
   [Server]
   public void SetDisplayName(string newDisplayName){
       displayName = newDisplayName;
@@ -27,6 +28,16 @@ public class MyNetworkPlayer : NetworkBehaviour
       playerColor = newPlayerColor;
   }
 
+  [Command]
+  private void CmdSetDisplayName(string newDisplayName){
+    RpcLogNewName(newDisplayName);
+    SetDisplayName(newDisplayName);
+  }
+
+  
+  #endregion
+
+  #region Client
   private void HandleDisplayColorUpdate(Color oldColor, Color newColor){
     displayColorRenderer.material.SetColor("_BaseColor", newColor);
   }
@@ -34,4 +45,17 @@ public class MyNetworkPlayer : NetworkBehaviour
   private void HandleDisplayNameUpdate(string oldDisplayName, string newDisplayName){
     displayNameText.text = newDisplayName;
   }
+
+  [ContextMenu("Set My Name")]
+  private void SetMyName(){
+    CmdSetDisplayName("My New Name");
+  }
+
+ [ClientRpc]
+  private void RpcLogNewName(string newDisplayName){
+   Debug.Log(newDisplayName);
+  }
 }
+
+
+#endregion
