f2c9de3e48346e943a7c94cef8a5a57307d0c242
QingOu <qingqingou@gmail.com>
2022-01-30 11:01:14 -0500

    Add variable and sync color

--- Assets/Scripts/MyNetworkManager.cs
+++ Assets/Scripts/MyNetworkManager.cs
@@ -5,17 +5,15 @@ using UnityEngine;
 
 public class MyNetworkManager : NetworkManager
 {
-    public override void OnClientConnect()
-    {
-        base.OnClientConnect();
-        Debug.Log("I connected to a server!");
-    }
-
     public override void OnServerAddPlayer(NetworkConnection conn)
     {
         base.OnServerAddPlayer(conn);
+        var player = conn.identity.GetComponent<MyNetworkPlayer>();
+
+        player.SetDisplayName($"Player {numPlayers}");
         
-        Debug.Log("We have a new player!");
-        Debug.Log($"Active Players Online: {this.numPlayers}");
+        var randomPlayerColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        player.SetPlayerColor(randomPlayerColor);
+  
     }
 }
---
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class MyNetworkPlayer : NetworkBehaviour
+{
+  [SyncVar]
+  [SerializeField]
+  private string displayName ="Missing Name";
+  
+  [SyncVar]
+  [SerializeField]
+  private Color playerColor = Color.black;
+
+  [Server]
+  public void SetDisplayName(string newDisplayName){
+      displayName = newDisplayName;
+  }
+
+  [Server]
+  public void SetPlayerColor(Color newPlayerColor){
+      playerColor = newPlayerColor;
+  }
+}
